9c58e295dc7571cac86554594fefa586c25aa1f6
carsonb136 <carsonbutler@suu.edu>
2020-12-04 16:12:54 -0700

    refactor(final-project): Q8 date mod.
    Updated .txt as well.

--- HotelQueries.sql
+++ HotelQueries.sql
@@ -1,21 +1,21 @@
 -- 1. View hotels, their managers, and manager contact info
 
-SELECT H.HotelName, CONCAT(S.FirstName, ' ', S.LastName) AS "Manager Name", S.PhoneNumber, S.Email 
-FROM HOTEL H, STAFF S 
-WHERE H.HotelManagerID = S.StaffID; 
+SELECT H.HotelName, CONCAT(S.FirstName, ' ', S.LastName) AS "Manager Name", S.PhoneNumber, S.Email
+FROM HOTEL H, STAFF S
+WHERE H.HotelManagerID = S.StaffID;
 
 
 -- 2. View all rooms in a hotel
 
-SELECT * 
-FROM ROOM 
+SELECT *
+FROM ROOM
 WHERE HotelID = 1;
 
 
 -- 3. View rooms in a hotel by room type
 
-SELECT * 
-FROM ROOM 
+SELECT *
+FROM ROOM
 WHERE HotelID = 5 AND RoomType = "Single-Twin";
 
 
@@ -54,7 +54,7 @@ WHERE SYSDATE() BETWEEN RV.CheckIn AND RV.CheckOut;
 
 
 -- 7. View current reservations and reservation information by customer --> All active reservations for customer 11
- 
+
 SELECT RM.RoomID, RM.RoomNumber, RM.HotelID, HT.HotelName, RV.*
 FROM ROOM RM
 	LEFT JOIN ROOM_RESERVATION RR ON RM.RoomID = RR.RoomID
@@ -63,14 +63,14 @@ FROM ROOM RM
 WHERE RV.CustomerID = 11 AND SYSDATE() BETWEEN RV.CheckIn AND RV.CheckOut;
 
 
--- 8. View who is working at a hotel (shifts) and what position (staff) the employee is filling --> Distinct list of staff and thier positions at hotel 1
+-- 8. View who is working at a hotel (shifts) and what position (staff) the employee is filling --> Who supervised the 4th of July fireworks show this year at Hotel 1?
 
 SELECT HT.HotelID, SH.ShiftID, S.StaffID, SH.ShiftPosition, SH.ShiftStart, SH.ShiftEnd, CASE
-	WHEN SYSDATE() BETWEEN SH.ShiftStart AND SH.ShiftEnd
+	WHEN '2020-07-04 21:00:00' BETWEEN SH.ShiftStart AND SH.ShiftEnd
 		THEN 'Scheduled'
 		ELSE 'OFF'
 		END AS "Shift Status"
-FROM SHIFT SH 
+FROM SHIFT SH
 	LEFT JOIN HOTEL HT ON HT.HotelID = SH.HotelID
 	LEFT JOIN STAFF S ON S.StaffID = SH.StaffID
 WHERE HT.HotelID = 1;
